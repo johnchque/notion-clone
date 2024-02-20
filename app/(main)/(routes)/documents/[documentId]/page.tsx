dcf64e217b0026f6017a7ee2e1469e525096985f
John Choque <johnchque.tech@gmail.com>
2024-02-20 23:59:24 +0100

    Adding blockeditor for documents.

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -9,6 +9,7 @@ import { Id } from "@/convex/_generated/dataModel";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Toolbar } from "@/components/toolbar";
 import { Cover } from "@/components/cover";
+import { Editor } from "@/components/editor";
 
 interface DocumentIdPageProps {
   params: {
@@ -27,8 +28,29 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
     documentId: params.documentId,
   });
 
+  const update = useMutation(api.documents.update);
+
+  const onChange = (content:string) => {
+    update({
+      id: params.documentId,
+      content
+    })
+  }
+
   if (document === undefined) {
-    return <div>Loading</div>;
+    return (
+      <div>
+        <Cover.Skeleton />
+        <div className="md:max-2-3xl lg:max-w-4xl mx-auto mt-10">
+          <div className="space-y-4 pl-8 pt-4">
+            <Skeleton className="h-14 w-[50%]" />
+            <Skeleton className="h-14 w-[80%]" />
+            <Skeleton className="h-14 w-[40%]" />
+            <Skeleton className="h-14 w-[60%]" />
+          </div>
+        </div>
+      </div>
+    );
   }
 
   if (document === null) {
@@ -40,6 +62,7 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
       <Cover url={document.coverImage} />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar initialData={document} />
+        <Editor onChange={onChange} initialContent={document.content}/>
       </div>
     </div>
   );
